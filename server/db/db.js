6dfad2a6d2b77bf70a9cdcc9f2c486484bf2f62a
alex <jiraiya.sama@gmail.com>
2021-09-22 16:55:11 -0500

    mongoose fix

--- server/db/db.js
+++ server/db/db.js
@@ -1,3 +1,3 @@
 const mongoose = require('mongoose');
 
-mongoose.connect('mongodb+srv://mongodbadmin:BIg6HVXSCDDYgvt5@cluster0.fqlwb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
+mongoose.connect('mongodb+srv://mongoadmin:<password>@cluster0.fqlwb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority');
